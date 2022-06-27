edab9fe514aa556271ea9d31fe6badba54458ab8
Nada Hasni <nadasthing@gmail.com>
2022-06-27 12:32:01 +0700

    add inertia loading indicator

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,18 @@ import React from "react";
 import { render } from "react-dom";
 import { createInertiaApp } from "@inertiajs/inertia-react";
 import "bootstrap";
+import { InertiaProgress } from "@inertiajs/progress";
 
 createInertiaApp({
     resolve: (name) => require(`./Pages/${name}`),
     setup({ el, App, props }) {
         render(<App {...props} />, el);
     },
+});
+
+InertiaProgress.init({
+    delay: 0,
+    showSpinner: true,
+    includeCSS: true,
+    color: "#ffffff",
 });
